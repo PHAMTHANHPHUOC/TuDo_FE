65aa68018b625b431efd202b70497c305e37284c
pham phuoc <thanhphuocj3@gmail.com>
2024-04-12 17:11:31 +0700

    auth

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 // import Default from './Layout/Wrapper/MasterRocker.vue'
-import Auth from './Layout/Wrapper/auth.vue'
+// import Auth from './Layout/Wrapper/auth.vue'
 import KhachHang from './Layout/Wrapper/MasterKhachHang.vue'
 import Admin from './layout/wrapper/MasterAdmin.vue'
 
@@ -10,7 +10,7 @@ import Toaster from "@meforma/vue-toaster";
 const app = createApp(App)
 app.use(router,Toaster)
 // app.component("default-layout", Default);
-app.component("auth-layout", Auth);
+// app.component("auth-layout", Auth);
 app.component("kh-layout", KhachHang);
 app.component("adm-layout", Admin);
 
