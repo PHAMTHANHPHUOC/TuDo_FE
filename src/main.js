bbe8d5bd7a92a744a4f559e1e4e42506c4423b4f
pham phuoc <thanhphuocj3@gmail.com>
2024-04-12 17:17:53 +0700

    router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
 // import Default from './Layout/Wrapper/MasterRocker.vue'
 // import Auth from './Layout/Wrapper/auth.vue'
 import KhachHang from './Layout/Wrapper/MasterKhachHang.vue'
